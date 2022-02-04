164131991061c05ab9f51c0248d05a5145886ef3
c0qu1 <ald52sunshine@gmail.com>
2022-02-04 16:33:23 -0500

    Moving the VIA json file to the via folder. Bugfixes to the ey191.json file to correct errors. Fixes for ey191.h to correct the layout/matrix mapping. Edited the keymap.c file to change Insert, Home, End and PGUP,PGDN.

--- keyboards/handwired/ey191/ey191.h
+++ keyboards/handwired/ey191/ey191.h
@@ -16,17 +16,17 @@
  * represents the switch matrix.
  */
 #define LAYOUT_ansi65x( \
-    K01, K02, K03, K04, K05,                               K0C,           K0F, K0G, \
-    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D,      K1F, K1G, \
-    K21,      K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, K2G, \
-    K31,      K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E,      K3G, \
-         K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C,      K4E, K4F, K4G, \
-    K51, K52,      K54,           K57,                K5B, K5C, K5D, K5E, K5F, K5G  \
+    K00, K01, K02, K03, K04,                               K0B,           K0E, K0F, \
+    K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C,      K1E, K1F, \
+    K20,      K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, \
+    K30,      K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D,      K3F, \
+         K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B,      K4D, K4E, K4F, \
+    K50, K51,      K53,           K56,                K5A, K5B, K5C, K5D, K5E, K5F  \
 ) { \
-   {K01, K02, K03, K04, K05, ___, ___, ___, ___, ___, ___, K0C, ___, ___, K0F, K0G}, \
-   {K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, K1D, ___, K1F, K1G}, \
-   {K21, ___, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F, K2G}, \
-   {K31, ___, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, K3E, ___, K3G}, \
-   {___, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, K4C, ___, K4E, K4F, K4G}, \
-   {K51, K52, ___, K54, ___, ___, K57, ___, ___, ___, K5B, K5C, K5D, K5E, K5F, K5G } \
+   {K00, K01, K02, K03, K04, ___, ___, ___, ___, ___, ___, K0B, ___, ___, K0E, K0F}, \
+   {K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, ___, K1E, K1F}, \
+   {K20, ___, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, K2D, K2E, K2F}, \
+   {K30, ___, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C, K3D, ___, K3F}, \
+   {___, K41, K42, K43, K44, K45, K46, K47, K48, K49, K4A, K4B, ___, K4D, K4E, K4F}, \
+   {K50, K51, ___, K53, ___, ___, K56, ___, ___, ___, K5A, K5B, K5C, K5D, K5E, K5F} \
 }
