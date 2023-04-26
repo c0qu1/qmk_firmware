bb4cb4aee03041056fd7e6c25bb54a3527af2a74
c0qu1 <ald52sunshine@gmail.com>
2023-04-26 00:06:27 -0400

    Added rp2040 bootloader reset on key press. Reversed the order of the columns to correct matrix order. Assigned bootmagic to specific key.

--- keyboards/handwired/retrorobot_macropad/config.h
+++ keyboards/handwired/retrorobot_macropad/config.h
@@ -17,5 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP7 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+
 #define ENCODERS_PAD_A { GP20, GP18 }
 #define ENCODERS_PAD_B { GP21, GP19 }
