62ac5d183d2f467a6319e9c26c503e205897073c
c0qu1 <ald52sunshine@gmail.com>
2023-04-25 22:05:35 -0400

    Added source of big_led to compilation in rules.mk, uncommented function in keymap.c.

--- keyboards/handwired/retrorobot_macropad/keymaps/default/keymap.c
+++ keyboards/handwired/retrorobot_macropad/keymaps/default/keymap.c
@@ -43,21 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-//layer_state_t layer_state_set_user(layer_state_t state) {
-//    if (get_highest_layer(state) == 1) {
-//        set_big_LED_rgb(LED_ON, LED_OFF, LED_OFF);
-//    } else if (get_highest_layer(state) == 2) {
-//        set_big_LED_rgb(LED_OFF, LED_ON, LED_OFF);
-//    } else {
-//        set_big_LED_rgb(LED_OFF, LED_OFF, LED_ON);
-//    }
-//    return state;
-//}
-
-// Use Big LED to show status
-//bool led_update_user(led_t led_state) {
-//    set_big_LED_r(led_state.caps_lock ? LED_ON : LED_OFF);
-//    set_big_LED_g(led_state.num_lock ? LED_ON : LED_OFF);
-//    set_big_LED_b(led_state.scroll_lock ? LED_ON : LED_OFF);
-//    return true;
-//}
+layer_state_t layer_state_set_user(layer_state_t state) {
+    if (get_highest_layer(state) == _1) {
+        set_big_LED_rgb(LED_ON, LED_OFF, LED_OFF);
+    } else if (get_highest_layer(state) == _2) {
+        set_big_LED_rgb(LED_OFF, LED_ON, LED_OFF);
+    } else {
+        set_big_LED_rgb(LED_OFF, LED_OFF, LED_ON);
+    }
+    return state;
+}
